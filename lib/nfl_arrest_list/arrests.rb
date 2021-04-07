6b483f18af7c697431217832b3fe9adf9c59d4f1
RobertBates94 <bates.to.infinity@gmail.com>
2021-04-06 20:11:52 -0500

    First Upload

--- lib/nfl_arrest_list/arrests.rb
+++ lib/nfl_arrest_list/arrests.rb
@@ -1,3 +1,8 @@
 class Arrests
-    attr_accessor :Team_name, 
+    attr_accessor :Team_preffered_name, :Name, :Category
+    
+    def initialize(Category, Name, Team_preffered_name)
+        @Category = Category
+        @name = Name
+        @Team_preffered_name = Team_preffered_name
 end
