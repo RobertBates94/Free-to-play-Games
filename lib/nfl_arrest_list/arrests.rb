a98919f45ae52386ab41975e2d4e569de778c92e
RobertBates94 <bates.to.infinity@gmail.com>
2021-04-07 11:34:50 -0500

    Commit

--- lib/nfl_arrest_list/arrests.rb
+++ lib/nfl_arrest_list/arrests.rb
@@ -1,9 +1,20 @@
 class Arrests
     attr_accessor :Team_preffered_name, :Name, :Category
+
+    @@all = []
     
     def initialize(Category, Name, Team_preffered_name)
         @Category = Category
         @name = Name
         @Team_preffered_name = Team_preffered_name
     end
+
+    def self.all
+        @@all
+    end
+
+    def save 
+        @@all << self
+    end
+
 end
